145cd339962d318bb327722d915e745259df89e6
justanothermanoj <justanothermanoj@gmail.com>
2015-12-19 00:05:10 +0530

    Improved clear function

--- js/TT.js
+++ js/TT.js
@@ -1,31 +1,38 @@
 // Termitext version 0.0.1
-
 function output(data) {
     document.querySelector("#output").innerHTML += "<p>" + data + "</p>"
 }
 
 function clear() {
     var output_window = document.querySelector("#output");
-    var all_p = output_window.querySelectorAll("p");
-
-    for(var i = 0; i < all_p.length; i++) {
-    	all_p[i].parentNode.removeChild(all_p[i]);
-    }
+    var parent = output_window.parentNode;
+    parent.removeChild(output_window);
+    var new_window = document.createElement("div");
+    new_window.id = 'output';
+    new_window.className = "full_height full_width";
+    parent.appendChild(new_window);
 }
 
 function run(func) {
 
     var editor_input = document.querySelector("#editor_input");
-    var output_window = document.querySelector("#output");
+    var main_window = document.querySelector("#Main_Window");
     var preScript = document.querySelector("#Main_Script");
     if (preScript) {
-        output_window.removeChild(preScript);
+        main_window.removeChild(preScript);
     }
     var script = document.createElement('script');
     script.id = "Main_Script";
-    script.innerHTML = " function main () { " + editor_input.value + " }";
-    output_window.appendChild(script);
+    script.innerHTML = " function main () { clear(); " + editor_input.value + " }";
+    main_window.appendChild(script);
 
     document.getElementById("main_btn").onclick();
 
 }
+
+var base = window.console.log;
+window.console.log = function(message) {
+    output(message);
+    return base.apply(this, arguments);
+};
+
